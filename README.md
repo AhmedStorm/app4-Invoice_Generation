18af9ac7ce78f19c59244965aee66ed5048d4d1a
Barhoma <ahmedtbarhoma@gmail.com>
2023-07-22 19:04:10 +0300

    Creating PDFs and the first stops of invoices

---
+++ README.md
@@ -0,0 +1,4 @@
+# _What is this project about?_ 
+
+*This is an App that creates PDF invoices from Excel Files.*
+
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import glob
-
+from fpdf import FPDF
+import pathlib
 filepaths = glob.glob("invoices/*.xlsx")
-print(filepaths)
+# print(filepaths)
 
 for item in filepaths:
-    df = pd.read_excel(item,sheet_name="Sheet 1")
-    print(df)
+    df = pd.read_excel(item, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filepath = pathlib.Path(item).stem
+    invoice = filepath.split('-')[0]
+    date = filepath.split('-')[1]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice}")
+    pdf.ln()
+    pdf.cell(w=50, h=8, txt=f"Date {date}")
+    pdf.output(f"PDFs/{filepath}.pdf")
