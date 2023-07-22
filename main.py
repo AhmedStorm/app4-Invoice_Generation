155c42c62f930bb49fb2123d826dc47448322a50
Barhoma <ahmedtbarhoma@gmail.com>
2023-07-22 22:43:40 +0300

    Adding Header & Content of the Tables

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 import pathlib
+
 filepaths = glob.glob("invoices/*.xlsx")
-# print(filepaths)
 
 for item in filepaths:
-    df = pd.read_excel(item, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     filepath = pathlib.Path(item).stem
-    invoice = filepath.split('-')[0]
-    date = filepath.split('-')[1]
+    invoice, date = filepath.split('-')
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+    pdf.ln(5)
+    df = pd.read_excel(item, sheet_name="Sheet 1")
+    columns = list(df.columns)
+    columns = [item.replace("_", " ").title() for item in columns]
+    #Add Headers of the table
+    pdf.set_font(family="Times", size=10, style="B")
+    we = [30, 60, 40, 30, 20]
+    for i, col in enumerate(columns):
+        pdf.cell(w=we[i], h=8, txt=col, border=1)
     pdf.ln()
-    pdf.cell(w=50, h=8, txt=f"Date {date}")
+    #Add the content of the table
+    pdf.set_font(family="Times", size=10)
+    for index, row in df.iterrows():
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
     pdf.output(f"PDFs/{filepath}.pdf")
